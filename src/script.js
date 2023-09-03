f34cf63b176f60966e992db8edc85c3607018f7b
vovkatom <36505066+vovkatom@users.noreply.github.com>
2023-09-03 18:00:30 +0300

    ddd

--- src/script.js
+++ src/script.js
@@ -1,3 +1,35 @@
+// $(document).ready(function(){
+// 	$('.slider').slick({
+// 		arrows:true,
+// 		dots:false,
+// 		slidesToShow:2,
+// 		slidesToScroll: 1,
+// 		autoplay:true,
+// 		speed:1000,
+// 		autoplaySpeed:800,
+// 		rows: 2,
+// 		slidesPerRow: 2,
+// 		vertical: true,
+// 		responsive:[
+// 			{
+// 				breakpoint: 768,
+// 				settings: {
+// 					slidesToShow:2
+// 				}
+// 			},
+// 			{
+// 				breakpoint: 550,
+// 				settings: {
+// 					slidesToShow:1
+// 				}
+// 			}
+// 		]
+// 	});
+// });
+
+
+
+
 $(document).ready(function(){
 	$('.slider').slick({
 		arrows:true,
@@ -24,5 +56,8 @@ $(document).ready(function(){
 				}
 			}
 		]
+
+
+		
 	});
 });
