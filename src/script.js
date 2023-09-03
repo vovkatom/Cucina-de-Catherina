8ad75822d40455beb10337e3d46b3ba2241f93f0
vovkatom <36505066+vovkatom@users.noreply.github.com>
2023-09-03 18:56:37 +0300

    ttt

--- src/script.js
+++ src/script.js
@@ -1,42 +1,10 @@
-// $(document).ready(function(){
-// 	$('.slider').slick({
-// 		arrows:true,
-// 		dots:false,
-// 		slidesToShow:2,
-// 		slidesToScroll: 1,
-// 		autoplay:true,
-// 		speed:1000,
-// 		autoplaySpeed:800,
-// 		rows: 2,
-// 		slidesPerRow: 2,
-// 		vertical: true,
-// 		responsive:[
-// 			{
-// 				breakpoint: 768,
-// 				settings: {
-// 					slidesToShow:2
-// 				}
-// 			},
-// 			{
-// 				breakpoint: 550,
-// 				settings: {
-// 					slidesToShow:1
-// 				}
-// 			}
-// 		]
-// 	});
-// });
-
-
-
-
 $(document).ready(function(){
 	$('.slider').slick({
 		arrows:true,
 		dots:false,
 		slidesToShow:2,
     slidesToScroll: 1,
-		autoplay:true,
+		autoplay:false,
 		speed:2000,
 		autoplaySpeed:2000,
     vertical: true,
